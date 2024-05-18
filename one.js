efa7d599d4038bb65346a805570df66109bf074b
bardwaz <anirudhbhardwaj.code@gmail.com>
2024-05-18 16:10:14 +0530

    fixed user images

--- one.js
+++ one.js
@@ -14,24 +14,17 @@ fetch('https://coding-week-2024-api.onrender.com/api/data')
         let i = 0;
         let str = ``;
 
-        // the svg files needed these cords to adjust to position
-        let viewcords = ["-25 -454 500 500", "-23 -439 250 500", "-26 -194 250 250", "-26 -195 250 250"];
-
         while (data[i] != undefined) {
             if (i < 4) {
                 let d = document.querySelector(`#big${i + 1}`);
                 d.innerHTML = `<div class="proTags" id="proT1">Featured</div>
                             <div class="proTags" id="proT2">${data[i].type}</div>
                             
-                            
                             <div class="NewsTitle">${data[i].headline}</div>
                             
                             <div class="auth">${data[i].authfirstname}</div>
-                            <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="${viewcords[i]}"
-                                    stroke-width="1.5" stroke="white" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                </svg></div>
+                            
+                            <div><img src='images/user.png' class='objimg'></div>
                             
                             <div class="datepub">${data[i].date}</div>`;
 
